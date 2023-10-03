39e8a4e473a4eb204d7aaafb96db7f0d6b6f505f
sonal <sonal.cmendis@gmail.com>
2023-10-03 17:38:39 +0530

    testing pipeline

--- src/pages/index.js
+++ src/pages/index.js
@@ -165,8 +165,8 @@ export default function Home(props) {
             <div className={`${homeStyles["desktop-text-container"]}`}>
               <h2>Stepping out of the comfort zone</h2>
               <p>
-                TEST. SECOND TEST Aaqil fearlessly captures the essence of city
-                life as well as the essence of nature through his lens,
+                TEST. SECOND TEST RETEST Aaqil fearlessly captures the essence
+                of city life as well as the essence of nature through his lens,
                 unapologetically exploring the raw and unfiltered reality of the
                 streets as a passionate and talented photographer.
               </p>
